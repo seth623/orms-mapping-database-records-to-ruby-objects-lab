7a8f509a5078a8863126874509d4b5fa77e97458
seth <seth623@gmail.com>
2022-04-24 13:46:03 -0600

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,110 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    
+    student = self.new 
+
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+
+    student 
+
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students; 
+    SQL
+
+    all_student_info = DB[:conn].execute(sql)
+
+    all_student_info.map{ |s| self.new_from_db(s) }
+
   end
 
+  def self.all_students_in_grade_X(x)
+
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade == ?;
+    SQL
+
+    DB[:conn].execute(sql, x)
+
+  end 
+
+  def self.first_student_in_grade_10
+
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade == 10
+    ORDER BY id ASC;
+    SQL
+
+    student = DB[:conn].execute(sql)
+
+    student.map{ |s| self.new_from_db(s) }.first 
+
+  end
+
+  def self.first_X_students_in_grade_10(x)
+
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade == 10
+    LIMIT ?;
+    SQL
+
+    student_rows  = DB[:conn].execute(sql, x)
+
+    student_rows.map{ |s| self.new_from_db(s) }
+
+  end 
+
+  def self.students_below_12th_grade
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12;
+    SQL
+
+    student_rows = DB[:conn].execute(sql)
+
+    student_rows.map{ |s| self.new_from_db(s) }
+
+  end 
+
+  def self.all_students_in_grade_9
+
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade == 9;
+    SQL
+
+    DB[:conn].execute(sql)
+
+  end 
+
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name == name;
+    SQL
+
+    student_info = DB[:conn].execute(sql)
+
+    student_info.map{ |s| self.new_from_db(s) }.first
+
   end
   
   def save
